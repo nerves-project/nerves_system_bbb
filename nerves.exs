6a0f3494ba15e2740011afa3557e05672b622c61
Justin Schneck <jschneck@mac.com>
2016-04-02 18:26:07 -0400

    fix defconfig reference

--- nerves.exs
+++ nerves.exs
@@ -5,5 +5,5 @@ config :nerves_system_bbb, :nerves_env,
   build_platform: :nerves_system_br,
   bakeware: [target: "bbb", recipe: "nerves/bbb"],
   ext: [
-    defconfig: "src/elixir_defconfig"
+    defconfig: "nerves_defconfig"
   ]
