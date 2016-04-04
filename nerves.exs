e4740f6fcf9819b1baaddfe3294d74ce7283c424
Justin Schneck <jschneck@mac.com>
2016-04-04 16:41:01 -0400

    point build_platform to module

--- nerves.exs
+++ nerves.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :nerves_system_bbb, :nerves_env,
-  type:  :system,
-  build_platform: :nerves_system_br,
+  type: :system,
+  build_platform: Nerves.System.Platforms.BR,
   bakeware: [target: "bbb", recipe: "nerves/bbb"],
   ext: [
     defconfig: "nerves_defconfig"
