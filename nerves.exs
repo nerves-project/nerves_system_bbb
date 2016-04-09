771cd9ec0440751340b9d0cbf6496d5496d94cf8
Justin Schneck <jschneck@mac.com>
2016-04-09 19:54:02 -0400

    Updated nerves config and removed Config.in

--- nerves.exs
+++ nerves.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :nerves_system_bbb, :nerves_env,
   type: :system,
-  build_platform: Nerves.System.Platforms.BR,
   bakeware: [target: "bbb", recipe: "nerves/bbb"],
-  ext: [
+  build_platform: Nerves.System.Platforms.BR,
+  build_config: [
     defconfig: "nerves_defconfig"
   ]
