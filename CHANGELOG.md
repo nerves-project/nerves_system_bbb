883bf5c2e01f1b5cfbd165f59acc4aa6923e2ee2
Justin Schneck <jschneck@mac.com>
2016-05-16 11:46:28 -0400

    update nerves_system_br to v0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## v0.6.0
+  
+
 ## v0.5.0
 
   * Nerves.System.BR v0.4.1
--- mix.exs
+++ mix.exs
@@ -20,9 +20,9 @@ defmodule NervesSystemBbb.Mixfile do
   end
 
   defp deps do
-    [{:nerves_system, "~> 0.1.0"},
-    #{:nerves_system_br, "~> 0.4.1"},
-     {:nerves_system_br, github: "nerves-project/nerves_system_br"},
+    [{:nerves_system, "~> 0.1.2"},
+     {:nerves_system_br, "~> 0.5.0"},
+     #{:nerves_system_br, github: "nerves-project/nerves_system_br"},
      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.6.0"}]
   end
 
