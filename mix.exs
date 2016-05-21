6b36d6ce5ae39e1457d091945a079d35ad8a2da5
Justin Schneck <jschneck@mac.com>
2016-05-21 12:48:00 -0400

    updated nerves_system_br to v0.5.1

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule NervesSystemBbb.Mixfile do
 
   defp deps do
     [{:nerves_system, "~> 0.1.2"},
-     {:nerves_system_br, "~> 0.5.0"},
      #{:nerves_system_br, github: "nerves-project/nerves_system_br"},
+     {:nerves_system_br, "~> 0.5.1"},
      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.6.0"}]
   end
 
