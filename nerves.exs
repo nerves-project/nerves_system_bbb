203c8babc4808998b1469c508fb27c2e1a67c207
Justin Schneck <jschneck@mac.com>
2016-06-29 12:09:51 -0400

    support for fetching from pre

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,8 @@ config :nerves_system_bbb, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_bbb/releases/download/v#{version}/nerves_system_bbb-v#{version}.tar.gz"],
+    "https://github.com/nerves-project/nerves_system_bbb/releases/download/v#{version}/nerves_system_bbb-v#{version}.tar.gz",
+    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_bbb-#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
