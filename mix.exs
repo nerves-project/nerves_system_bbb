eb06b0bcabf8ed79ffab1a3b0bd95e9e7dbd5a31
Frank Hunleth <fhunleth@troodon-software.com>
2017-01-22 15:52:57 -0500

    Test build with new nerves_system_br

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule NervesSystemBbb.Mixfile do
 
   defp deps do
     [{:nerves, "~> 0.4.0"},
-     {:nerves_system_br, "~> 0.8.1"},
+      #{:nerves_system_br, "~> 0.8.1"},
+     {:nerves_system_br, github: "nerves-project/nerves_system_br", branch: "br-2016.11.1-bump"},
      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.9.0"}]
   end
 
