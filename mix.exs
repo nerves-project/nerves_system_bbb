36f012b845e483c02cc5bd3c2466d01c42ff58f4
Justin Schneck <jschneck@mac.com>
2017-07-29 11:21:30 -0400

    fix warnings for elixir 1.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule NervesSystemBbb.Mixfile do
 
   @version Path.join(__DIR__, "VERSION")
     |> File.read!
-    |> String.strip
+    |> String.trim
 
   def project do
     [app: :nerves_system_bbb,
