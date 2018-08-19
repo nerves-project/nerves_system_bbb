62500147c31d455d476ea871e992c0b29abaefe2
Justin Schneck <jschneck@mac.com>
2018-08-18 21:45:57 -0400

    Add provisioning.conf and include it in fwup.conf

--- post-build.sh
+++ post-build.sh
@@ -34,3 +34,6 @@ dd if=/dev/zero of=$TARGET_DIR/lib/firmware/ti-connectivity/wl127x-nvs.bin \
 # active firmware.
 mkdir -p $TARGET_DIR/usr/share/fwup
 $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_DIR/usr/share/fwup/revert.fw
+
+# Copy the fwup includes to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
