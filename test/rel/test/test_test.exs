9991feeddaf490e870bdba33d2ab7afb0b347a75
Justin Schneck <jschneck@mac.com>
2019-07-17 10:55:18 -0400

    Update system-test firmware

--- test/rel/test/test_test.exs
+++ test/rel/test/test_test.exs
@@ -1,9 +1,8 @@
 defmodule TestTest do
   use ExUnit.Case
-  doctest Test
 
   test "i2c interface exists" do
     assert File.exists?("/dev/i2c-0")
-    assert File.exists?("/dev/i2c-2")
+    assert File.exists?("/dev/i2c-1")
   end
 end
