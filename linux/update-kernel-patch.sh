25f1cc5fc6403f370b92bb04e5a6a0fa2c78a73a
LynnL4 <lht856@foxmail.com>
2021-03-17 14:29:12 +0800

    linux: add linux-4-19 config

--- linux/update-kernel-patch.sh
+++ linux/update-kernel-patch.sh
@@ -10,8 +10,7 @@ update_kernel_patch() {
   local KERNEL_VERSION=$2
   local OUTPUT_PATCH=$3
 
-  local ORIGINAL_DIFF=patch-$PATCH_VERSION.diff
-  local ORIGINAL_DIFF_XZ=$ORIGINAL_DIFF.xz
+  local KERNEL_PATCH=$PATCH_VERSION.tar.gz
   local KERNEL_TARBALL=linux-$KERNEL_VERSION.tar.xz
 
   # Work in a temporary directory
@@ -21,16 +20,12 @@ update_kernel_patch() {
 
   echo "Downloading files..."
 
-  local RCN_PATCH_URL
+  local PATCH_URL
   local KERNEL_URL
 
   case $KERNEL_VERSION in
-      5*)
-          RCN_PATCH_URL=http://rcn-ee.net/deb/bullseye-armhf/v$PATCH_VERSION/$ORIGINAL_DIFF_XZ
-          KERNEL_URL=https://www.kernel.org/pub/linux/kernel/v5.x/$KERNEL_TARBALL
-          ;;
       4*)
-          RCN_PATCH_URL=http://rcn-ee.net/deb/bullseye-armhf/v$PATCH_VERSION/$ORIGINAL_DIFF_XZ
+          PATCH_URL=https://github.com/beagleboard/linux/archive/$KERNEL_PATCH
           KERNEL_URL=https://www.kernel.org/pub/linux/kernel/v4.x/$KERNEL_TARBALL
           ;;
       *)
@@ -39,7 +34,7 @@ update_kernel_patch() {
   esac
 
   # Download the master patch file for the RCN kernel
-  wget $RCN_PATCH_URL
+  wget $PATCH_URL
 
   # Download the kernel that it will patch
   wget $KERNEL_URL
@@ -47,23 +42,12 @@ update_kernel_patch() {
   # Extract the kernel twice so that the patch can be compared
   echo "Extracting..."
   tar -x -f $KERNEL_TARBALL && mv linux-$KERNEL_VERSION a
-  tar -x -f $KERNEL_TARBALL && mv linux-$KERNEL_VERSION b
-
-  # Expand the "git" style patch
-  unxz $ORIGINAL_DIFF_XZ
-
-  # Make a git repo and apply the "git" style patch
-  echo "Creating temporary git repo to extract RCN patch..."
-  cd b
-  git init
-  git add .
-  git commit -q -m "Initial commit"
-  git apply ../$ORIGINAL_DIFF
-  rm -fr .git
+  tar -x -f $KERNEL_PATCH && mv linux-$PATCH_VERSION b
+ 
+  rm b/.github
 
   # Now create a regular "diff" patch
   echo "Creating patch..."
-  cd ..
   diff -Naur --no-dereference a b > ../$OUTPUT_PATCH || :
 
   # Clean up
@@ -75,6 +59,6 @@ update_kernel_patch() {
 }
 
 #update_kernel_patch 4.19.120-bone50 4.19.120 0001-rcn-linux-4.19.120-bone50.patch
-update_kernel_patch 5.4.52-bone31 5.4.52 0001-rcn-linux-5.4.52-bone31.patch
+update_kernel_patch 4.19.94-ti-r59 4.19.94 0001-rcn-linux-4.19.94-ti-r59.patch
 
 echo "Updated patches. Now rebuild the linux kernel."
