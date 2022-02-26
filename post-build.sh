dbd68636caf609829902ff99dc3cd3ee1146eaf0
Frank Hunleth <fhunleth@troodon-software.com>
2022-02-25 20:08:14 -0500

    Move Linux kernel to SquashFS
    The latest U-Boot's SquashFS support is much, much faster. This makes it
    possible to remove the hack of moving the Linux kernel from the SquashFS
    partition to the FATFS one. No more needing to write to the FATFS for
    firmware updates!
    
    FATFS read performance:
    
    2828616 bytes read in 185 ms (14.6 MiB/s)
    
    SquashFS read performance:
    
    2828616 bytes read in 204 ms (13.2 MiB/s)
    
    IMPORTANT: The main problem with this update is that if you have devices
    deployed with U-Boot 2018.07 (everything with nerves_system_bbb v2.13.2
    and earlier), then updating to this firmware works, but will be SLOW.

--- post-build.sh
+++ post-build.sh
@@ -37,10 +37,3 @@ $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_D
 
 # Copy the fwup includes to the images dir
 cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
-
-# Since U-Boot's squashfs support is so slow (sadly), store the Linux
-# kernel in the FAT filesystem instead of /boot. Everything else can
-# go in /boot since it's so small. See the fwup.conf for how the zImage
-# is put in the FAT filesystem. Remove zImage from /boot since we can't
-# tell Buildroot to just put the .dtb files there.
-rm -f $TARGET_DIR/boot/zImage
