4fc1a7b90344e8e557d6bab74e6a98e49d5ff6c9
Jean Parpaillon <jean.parpaillon@free.fr>
2022-08-03 19:12:22 +0200

    buildroot: add package bborg-overlays-zola

--- package/bborg-overlays-zola/bborg-overlays-zola.mk
+++ package/bborg-overlays-zola/bborg-overlays-zola.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 BBORG_OVERLAYS_ZOLA_VERSION = 8085119c8940312853590c3394c591b8a20388cf
-BBORG_OVERLAYS_ZOLA_SITE = $(call github,LynnL4,bb.org-overlays,$(BBORG_OVERLAYS_ZOLA_VERSION))
+BBORG_OVERLAYS_ZOLA_SITE = $(call github,OffgridElectric,bb.org-overlays,$(BBORG_OVERLAYS_ZOLA_VERSION))
 BBORG_OVERLAYS_ZOLA_LICENSE = GPLv2
 BBORG_OVERLAYS_ZOLA_DEPENDENCIES = host-dtc
 
