cd0dc572204b2814969dc36353df62d6a4e5a1e1
Jean Parpaillon <jean.parpaillon@free.fr>
2022-08-22 17:54:26 +0200

    Add test for CAN port ttyO3

--- test/rel/test/device_tree_test.exs
+++ test/rel/test/device_tree_test.exs
@@ -14,4 +14,20 @@ defmodule DeviceTreeTest do
       assert File.exists?("/proc/device-tree/chosen/overlays/BB-ZOLA-GATEWAY-00A0")
     end
   end
+
+  describe "CAN devices" do
+    @can_dev "ttyO3"
+
+    test "/dev/#{@can_dev} exists" do
+      assert File.exists? "/dev/#{@can_dev}"
+    end
+
+    test "/dev/ttyO3 is tty,type=250" do
+      sysfs_file = "/sys/class/tty/#{@can_dev}/dev"
+      assert File.exists? sysfs_file
+
+      ret = sysfs_file |> File.read!() |> String.split(":", parts: 2)
+      assert ["250", _] = ret
+    end
+  end
 end
