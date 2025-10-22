6b954ec84aa838b2d4e6af24ca47204422801c42
Frank Hunleth <fhunleth@troodon-software.com>
2025-10-22 17:58:45 -0400

    Switch kernel patch source to sid
    Even though the official Beaglebone images are Debian Trixie, they're
    getting their kernel patches from sid.

--- linux/update-kernel-patch.sh
+++ linux/update-kernel-patch.sh
@@ -11,7 +11,7 @@ update_kernel_patch() {
   local OUTPUT_PATCH=$3
 
   local ORIGINAL_DIFF=patch-$PATCH_VERSION.diff
-  local ORIGINAL_DIFF_XZ=$ORIGINAL_DIFF.xz
+  local ORIGINAL_DIFF_GZ=$ORIGINAL_DIFF.gz
   local KERNEL_TARBALL=linux-$KERNEL_VERSION.tar.xz
 
   # Work in a temporary directory
@@ -21,7 +21,7 @@ update_kernel_patch() {
 
   echo "Downloading files..."
 
-  RCN_PATCH_URL=http://rcn-ee.net/deb/bookworm-armhf/v$PATCH_VERSION/$ORIGINAL_DIFF_XZ
+  RCN_PATCH_URL=http://rcn-ee.net/deb/sid-armhf/v$PATCH_VERSION/$ORIGINAL_DIFF_GZ
   KERNEL_URL=https://www.kernel.org/pub/linux/kernel/v5.x/$KERNEL_TARBALL
 
   # Download the master patch file for the RCN kernel
@@ -36,7 +36,7 @@ update_kernel_patch() {
   tar -x -f $KERNEL_TARBALL && mv linux-$KERNEL_VERSION b
 
   # Expand the "git" style patch
-  unxz $ORIGINAL_DIFF_XZ
+  gunzip $ORIGINAL_DIFF_GZ
 
   # Make a git repo and apply the "git" style patch
   echo "Creating temporary git repo to extract RCN patch..."
